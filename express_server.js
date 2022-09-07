1523d1b63f610707fb9efc19b75c47e994dc5e2a
jan2052 <janet_lam@live.ca>
2022-09-07 18:35:30 -0400

    can now update database and redirect using short url

--- express_server.js
+++ express_server.js
@@ -14,9 +14,24 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// creates short url for longURL
 app.post("/urls", (req, res) => {
+  const id = generateRandomString()
+  urlDatabase[id] = req.body.longURL
+  res.redirect("/urls")
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  res.send('Ok'); // Respond with 'Ok' (we will replace this)
+});
+
+//redirects to url
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id]
+  if (longURL) {
+    res.redirect(longURL);
+    return;
+  }
+
+  res.send('404 Page not found');
 });
 
 app.get("/urls", (req, res) => {
@@ -29,7 +44,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[`${req.params.id}`]};
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
@@ -44,15 +59,18 @@ app.get("/hello", (req, res) => {
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
+});
+
+app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
- });
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-function generateRandomString() {}
+function generateRandomString() {
+  const randomString = Math.random().toString(36).substring(6)
+  return randomString
+}
 generateRandomString()
