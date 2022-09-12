7cfcb3a66d4b0df0cef5d4f2e9b72786c6e2f8d8
jan2052 <janet_lam@live.ca>
2022-09-12 18:01:46 -0400

    signin/singout buttons and displays username

---
+++ delete.js
@@ -0,0 +1,6 @@
+<% if (username) { %>
+  <li>Welcome <%= user.name %>
+  <% } else { %>  
+  <a class="navbar-brand" href="/login">Login</a></li>
+  <a class="navbar-brand" href="/register">Login</a></li>
+<% } %> 
--- express_server.js
+++ express_server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cookieParser = require('cookie-parser')
 const app = express();
+app.use(cookieParser())
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs")
@@ -10,8 +12,43 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+const userDb = {
+  '8da584': {
+    username: '8da584',
+    email: 'hello@gmail.com',
+    password: 'password',
+  },
+  '8da585': {
+    username: '8da585',
+    email: 'world@gmail.com',
+    password: 'password123',
+  }
+}
+
+// Login button not on main url page
+// Couldn't display username // user_id cookie not working (user not defined in)
+// 
+
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+// LOGIN
+app.post("/login", (req, res) => {
+  const {username, email, password } = req.body
+  
+  for (const key in userDb) {
+    if(Object.hasOwnProperty.call(userDb, key)) {
+      const dbUser = userDb[key];
+        if (dbUser.username === req.body) {
+          return res.status(403).send('Email is already taken')
+        }
+    }
+  }
+
+  console.log(userDb)
+  res.cookie('username',username)
+  res.redirect('/urls')
 });
 
 // creates short url for website and redirects user to database
@@ -20,6 +57,17 @@ app.post("/urls", (req, res) => {
   urlDatabase[id] = req.body.longURL
   res.redirect("/urls")
   console.log(req.body); // Log the POST request body to the console
+});
+
+// EDIT
+app.post("/urls/:id/edit", (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL
+  res.redirect("/urls");
+});
+// DELETE
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id]
+  res.redirect("/urls");
 });
 
 //redirects to url
@@ -32,18 +80,28 @@ app.get("/u/:id", (req, res) => {
   res.send('404 Page not found');
 });
 
+
+
+// Shows USERNAME when logged in
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  const username = req.cookies['username'];
+  // if(!username) return res.render('urls_index', { urls: urlDatabase, user: null});
+
+  // const currentUser = userDb[userId];
+  // if (!currentUser) return res.send('User not found');
+  // console.log(userId)
+
+  // const userObject = currentUser[userId]
+  // console.log(userObject)
+  
+  const templateVars = { urls: urlDatabase , username: username};
+  return res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id]
-  res.redirect("/urls");
+  const username = req.cookies['username'];
+  const templateVars = { urls: urlDatabase , username: username};
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -51,22 +109,34 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+//AUTH ENDPOINTS
+app.get("/urls", (req, res) => {
+  const templateVars = {user: null}
+  res.render("/urls", templateVars);
+});
+
+// LOGOUT
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('urls')
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
+// app.get("/set", (req, res) => {
+//   const a = 1;
+//   res.send(`a = ${a}`);
+// });
 
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
-});
+// app.get("/fetch", (req, res) => {
+//   res.send(`a = ${a}`);
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -76,7 +146,7 @@ function generateRandomString() {
   let randomString = '';
   let characters = '012345679abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   for (let i = 0; i < 6; i++) {
-      randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+    randomString += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return randomString;
 }
