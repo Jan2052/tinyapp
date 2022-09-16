7358f430e12a1cbd6834a5ea5c34a271044a0fd0
jan2052 <janet_lam@live.ca>
2022-09-16 18:07:26 -0400

    linted helperstest

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -6,29 +6,29 @@ const {
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
+    const user = getUserByEmail("user@example.com", testUsers);
     const expectedUserID = "userRandomID";
-    assert.equal(user, expectedUserID)
+    assert.equal(user, expectedUserID);
   });
 });
 
 describe('getUserByEmail', function() {
   it('should return undefined for a user that is not in our user database', function() {
-    const user = getUserByEmail("hello@example.com", testUsers)
+    const user = getUserByEmail("hello@example.com", testUsers);
     const expectedUserID = undefined;
-    assert.equal(user, expectedUserID)
+    assert.equal(user, expectedUserID);
   });
 });
